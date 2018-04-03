dd82a0f51f3a51e571c064b12a6f707c28367b5a
ophid1an <dermentzoglou.ioannis@gmail.com>
2018-04-03 14:52:39 +0300

    Implement basic statistics

--- src/main/scala/ProjectMMD.scala
+++ src/main/scala/ProjectMMD.scala
@@ -1,10 +1,9 @@
 import org.apache.spark.sql.SparkSession
 
+case class Basket(b: Array[String])
+
 object ProjectMMD {
   def main(args: Array[String]): Unit = {
-
-    println("***********************************************************************************************")
-
     // Create spark session
     val spark = SparkSession
       .builder()
@@ -12,12 +11,73 @@ object ProjectMMD {
       .master("local[*]")
       .getOrCreate()
 
-    // Suppress info messages
-    spark.sparkContext.setLogLevel("ERROR")
+    val sc = spark.sparkContext
 
-    println("Hello, Spark!")
+    // Suppress info messages
+    sc.setLogLevel("ERROR")
+
+    val groceriesFilename = "groceries.csv"
+    val productsFilename = "products-categorized.csv"
+
+    //    val basketsDF = spark.read.format("csv").load(groceriesFilename)
+    //    val productsDF = spark.read.format("csv").load(productsFilename)
+
+    val basketsRDD = sc.
+      textFile(groceriesFilename).
+      map(_.trim.split(',').
+        map(_.trim)
+      ).
+      cache()
+
+    val productsRDD = sc.
+      textFile(productsFilename).
+      map(_.trim.split(',').
+        map(_.trim)
+      ).
+      map(x =>
+        (x(0),
+          x(1).split('/').
+            map(_.trim)
+        )
+      ).
+      cache()
+
+
+    showStatistics()
+
     spark.stop()
 
-    println("***********************************************************************************************")
+    def showStatistics(): Unit = {
+      val basketsCnt = basketsRDD.count()
+      val basketsSizes = basketsRDD.map(_.size)
+      val productsCnt = productsRDD.count()
+
+      //  basketsRDD.reduce((x1, x2) => (x1.toBuffer ++= x2).toArray)
+      //  productsRDD.reduce((x1, x2) => (Array(x1(0)).toBuffer ++= Array(x2(0))).toArray)
+
+      println("**********")
+      println("Statistics")
+      println("**********")
+
+      println("\nBaskets")
+      println("------------------------------")
+      println("Count: " + basketsCnt)
+
+      println("First 5 baskets: ")
+      for (i <- basketsRDD.take(5)) println("\t" + i.mkString(", "))
+
+      println("First 5 baskets sizes: " + basketsSizes.take(5).mkString(", "))
+      println("Min size: " + basketsSizes.min())
+      println("Max size: " + basketsSizes.max())
+      println("Mean size: " + basketsSizes.mean())
+
+      println("\nProducts")
+      println("------------------------------")
+      println("Count: " + productsCnt)
+
+      println("First 5 products: ")
+      for (i <- productsRDD.take(5)) println("\t" + i._1 + " -> " + i._2.mkString(", "))
+
+    }
   }
 }
