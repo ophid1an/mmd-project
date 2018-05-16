73951235914ec58dbc44a775549313b45085137f
ophid1an <ophid1an@users.noreply.github.com>
2018-05-16 03:09:55 +0300

    Implement Taxonomy class and corresponding tests

---
+++ src/main/scala/project/product/Taxonomy.scala
@@ -0,0 +1,47 @@
+package project.product
+
+case class Taxonomy(products: Map[String, Int] = Map(), classes: Map[String, Int] = Map(),
+                    subClasses: Map[String, Int] = Map(),
+                    productsToSubClasses: Map[Int, Int] = Map(),
+                    subClassesToClasses: Map[Int, Int] = Map()
+                   ) {
+  lazy val subClassesToProducts: Map[Int, List[Int]] = invertMap(productsToSubClasses)
+  lazy val classesToSubClasses: Map[Int, List[Int]] = invertMap(subClassesToClasses)
+
+  def ++(other: Taxonomy): Taxonomy = {
+    if (other.products.size == 1 && other.classes.size == 1 && other.subClasses.size == 1
+      && other.subClassesToClasses.size == 1 && other.productsToSubClasses.size == 1) {
+      val newProducts = other.products.mapValues(_ => products.size) ++ products
+      val newClasses = other.classes.mapValues(_ => classes.size) ++ classes
+      val newSubClasses = other.subClasses.mapValues(_ => subClasses.size) ++ subClasses
+      val otherProductCorrectId = newProducts.getOrElse(other.products.keysIterator.next(), -1)
+      val otherClassCorrectId = newClasses.getOrElse(other.classes.keysIterator.next(), -1)
+      val otherSubClassCorrectId = newSubClasses.getOrElse(other.subClasses.keysIterator.next(), -1)
+
+      this.copy(products = newProducts, classes = newClasses, subClasses = newSubClasses,
+        productsToSubClasses = productsToSubClasses.updated(otherProductCorrectId, otherSubClassCorrectId),
+        subClassesToClasses = subClassesToClasses.updated(otherSubClassCorrectId, otherClassCorrectId)
+      )
+    }
+    else
+      sys.error("Taxonomy.++ operates on Taxonomy object with one product.")
+  }
+
+  private def invertMap[A, B](inputMap: Map[A, B]): Map[B, List[A]] = {
+    inputMap.foldLeft(Map[B, List[A]]()) {
+      case (mapAccumulator, (value, key)) =>
+        if (mapAccumulator.contains(key)) {
+          mapAccumulator.updated(key, mapAccumulator(key) :+ value)
+        } else {
+          mapAccumulator.updated(key, List(value))
+        }
+    }
+  }
+}
+
+object Taxonomy {
+  def apply(prodName: String, seq: Seq[String]): Taxonomy = seq match {
+    case Seq(a) => apply(Map(prodName -> 0), Map(a -> 0), Map(a -> 0), Map(0 -> 0), Map(0 -> 0))
+    case Seq(a, b, _*) => apply(Map(prodName -> 0), Map(a -> 0), Map(b -> 0), Map(0 -> 0), Map(0 -> 0))
+  }
+}
---
+++ src/test/scala/TaxonomySpec.scala
@@ -0,0 +1,69 @@
+import org.scalatest.FunSuite
+import project.product.Taxonomy
+
+class TaxonomySpec extends FunSuite {
+
+  val t0 = Taxonomy()
+  val t1 = Taxonomy("a", List("a1"))
+  val t2 = Taxonomy("b", List("b1"))
+  val t3 = Taxonomy("c", List("b1", "b2"))
+  val t1_2 = Taxonomy(Map("a" -> 0, "b" -> 1), Map("a1" -> 0, "b1" -> 1), Map("a1" -> 0, "b1" -> 1),
+    Map(0 -> 0, 1 -> 1), Map(0 -> 0, 1 -> 1))
+  val t1_2_3 = Taxonomy(Map("a" -> 0, "b" -> 1, "c" -> 2), Map("a1" -> 0, "b1" -> 1),
+    Map("a1" -> 0, "b1" -> 1, "b2" -> 2), Map(0 -> 0, 1 -> 1, 2 -> 2), Map(0 -> 0, 1 -> 1, 2 -> 1))
+
+  test("t0.ProductsToSubClasses") {
+    assert(t0.productsToSubClasses == Map())
+  }
+
+  test("t0.subClassesToClasses") {
+    assert(t0.subClassesToClasses == Map())
+  }
+
+  test("t0.subClassesToProducts") {
+    assert(t0.subClassesToProducts == Map())
+  }
+
+  test("t0.classesToSubClasses") {
+    assert(t0.classesToSubClasses == Map())
+  }
+
+  test("t1_2.subClassesToProducts") {
+    assert(t1_2.subClassesToProducts == Map(0 -> List(0), 1 -> List(1)))
+  }
+
+  test("t1_2.classesToSubClasses") {
+    assert(t1_2.classesToSubClasses == Map(0 -> List(0), 1 -> List(1)))
+  }
+
+  test("t1_2_3.subClassesToProducts") {
+    assert(t1_2_3.subClassesToProducts == Map(0 -> List(0), 1 -> List(1), 2 -> List(2)))
+  }
+
+  test("t1_2_3.classesToSubClasses") {
+    assert(t1_2_3.classesToSubClasses == Map(0 -> List(0), 1 -> List(1, 2)))
+  }
+
+  test("t0 ++ t1 == t1") {
+    assert(t0 ++ t1 == t1)
+  }
+
+  test("t1 ++ t1 == t1") {
+    assert(t1 ++ t1 == t1)
+  }
+
+  test("t1 ++ t2 == t1_2") {
+    assert(t1 ++ t2 == t1_2)
+  }
+
+  test("t1_2 ++ t3 == t1_2_3") {
+    assert(t1_2 ++ t3 == t1_2_3)
+  }
+
+  test("t1 ++ t1_2 should produce RunTimeException") {
+    assertThrows[RuntimeException] {
+      t1 ++ t1_2
+    }
+  }
+}
+
