92ca231eb0b69434ede12f3fd51d46ec2de204dd
ophid1an <ophid1an@users.noreply.github.com>
2018-06-03 16:15:02 +0300

    Output stuff to Map[String, Double]

--- src/main/scala/project/ProjectMMD.scala
+++ src/main/scala/project/ProjectMMD.scala
@@ -2,6 +2,9 @@ package project
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.mllib.fpm.FPGrowth
+import org.apache.spark.mllib.fpm.AssociationRules
+import collection.breakOut
 import project.customer.{Customer, Spending}
 import project.product.{Product, Taxonomy}
 
@@ -25,6 +28,7 @@ object ProjectMMD {
 
     val seed = 1
     val rand = new scala.util.Random(seed)
+    val sampleSize = 5
 
     val (groceriesFilename, productsFilename, customersMaxCard) =
       if (testing) ("groceries-testing.csv", "products-categorized-testing.csv", 3)
@@ -69,10 +73,10 @@ object ProjectMMD {
       println("------------------------------")
       println("Count: " + basketsCnt)
 
-      println("First 5 baskets: ")
-      for (i <- baskets.take(5)) println("\t" + i.mkString(", "))
+      println("Baskets sample: ")
+      for (i <- baskets.take(sampleSize)) println("\t" + i.mkString(", "))
 
-      println("First 5 baskets sizes: " + basketsSizes.take(5).mkString(", "))
+      println("Baskets sample sizes: " + basketsSizes.take(sampleSize).mkString(", "))
       println("Min size: " + basketsSizes.min())
       println("Max size: " + basketsSizes.max())
       println("Mean size: " + basketsSizes.mean())
@@ -81,8 +85,8 @@ object ProjectMMD {
       println("------------------------------")
       println("Count: " + productsCnt)
 
-      println("First 5 products: ")
-      for (i <- products.take(5)) println("\t" + i._1 + " -> " + i._2.mkString(", "))
+      println("Products sample: ")
+      for (i <- products.take(sampleSize)) println("\t" + i._1 + " -> " + i._2.mkString(", "))
     }
 
     def computeCustomers
@@ -92,12 +96,18 @@ object ProjectMMD {
       val taxonomy = productsRDD.collect().foldLeft(Taxonomy())((acc, i) => acc ++ Taxonomy(i._1, i._2))
       val productsToSubClassesB = sc.broadcast(taxonomy.productsToSubClasses)
       val productsB = sc.broadcast(taxonomy.products)
+      val subClassesToClassesB = sc.broadcast(taxonomy.subClassesToClasses)
 
       val transformedBasketsRDD = basketsRDD
         .map(b => b.map(bItem => productsToSubClassesB.value.getOrElse(productsB.value.getOrElse(bItem, -1), -1)))
 
       val assignedCustomersRDD = transformedBasketsRDD
-        .map(b => Customer(getRandomId, Spending[Int]() ++ Spending(b: _*)))
+        .map(b => {
+          val clArr = b.map(subClassesToClassesB.value.getOrElse(_, -1))
+          Customer(getRandomId,
+            Spending[Int]() ++ Spending(clArr: _*),
+            Spending[Int]() ++ Spending(b: _*))
+        })
         .map(customer => customer.id -> customer)
 
       val customersRDD = assignedCustomersRDD.reduceByKey(_ ++ _)
@@ -105,54 +115,92 @@ object ProjectMMD {
       val customers = customersRDD.collect().toMap
 
       assert(basketsRDD.map(_.length).sum ==
-        customers.values.foldLeft(0.0)(_ + _.spending.vec.values.sum)
+        customers.values.foldLeft(0.0)(_ + _.clSpending.vec.values.sum)
       )
 
       assert(basketsRDD.map(_.length).sum ==
-        customers.values.foldLeft(0.0)(_ + _.spending.cnt)
+        customers.values.foldLeft(0.0)(_ + _.subClSpending.vec.values.sum)
+      )
+
+      assert(basketsRDD.map(_.length).sum ==
+        customers.values.foldLeft(0.0)(_ + _.clSpending.cnt)
+      )
+
+      assert(basketsRDD.map(_.length).sum ==
+        customers.values.foldLeft(0.0)(_ + _.subClSpending.cnt)
       )
 
       val fractionalCustomers = customers.mapValues(_.fractional)
 
       val customersCard = customers.size
 
-      assert(customersCard ==
-        fractionalCustomers.values.foldLeft(0.0)(_ + _.spending.vec.values.sum)
+      println("Customers card: " + customersCard)
+      println("Folded clSpending: " + fractionalCustomers.values.foldLeft(0.0)(_ + _.clSpending.vec.values.sum))
+      println("Folded subClSpending: " + fractionalCustomers.values.foldLeft(0.0)(_ + _.subClSpending.vec.values.sum))
+
+      assert(Math.abs(customersCard -
+        fractionalCustomers.values.foldLeft(0.0)(_ + _.clSpending.vec.values.sum)) < 0.00001
       )
 
-      val fractionalSpendingsTotal = fractionalCustomers.values
-        .map(_.spending).reduce(_ ++ _)
+      assert(Math.abs(customersCard -
+        fractionalCustomers.values.foldLeft(0.0)(_ + _.subClSpending.vec.values.sum)) < 0.00001
+      )
 
-      val adjustedFractionalSpendingsTotal = fractionalSpendingsTotal.vec
+
+      val fractionalClSpendingsTotal = fractionalCustomers.values
+        .map(_.clSpending).reduce(_ ++ _)
+
+      val fractionalSubClSpendingsTotal = fractionalCustomers.values
+        .map(_.subClSpending).reduce(_ ++ _)
+
+      val adjustedFractionalClSpendingsTotal = fractionalClSpendingsTotal.vec
+        .mapValues(customersCard / _)
+
+      val adjustedFractionalSubClSpendingsTotal = fractionalSubClSpendingsTotal.vec
         .mapValues(customersCard / _)
 
       val normalizedFractionalCustomers = fractionalCustomers.mapValues(
-        c => Customer(c.id, c.spending * adjustedFractionalSpendingsTotal)
+        c => Customer(c.id,
+          c.clSpending * adjustedFractionalClSpendingsTotal,
+          c.subClSpending * adjustedFractionalSubClSpendingsTotal)
       )
 
+      //      (customers, fractionalCustomers, fractionalCustomers)
       (
-        customers.mapValues(c => Customer(c.id, Spending(c.spending.vec.map {
-          case (k, v) => taxonomy.idsToSubClasses.getOrElse(k, "") -> v
-        }))),
-        fractionalCustomers.mapValues(c => Customer(c.id, Spending(c.spending.vec.map {
-          case (k, v) => taxonomy.idsToSubClasses.getOrElse(k, "") -> v
-        }))),
-        normalizedFractionalCustomers.mapValues(c => Customer(c.id, Spending(c.spending.vec.map {
-          case (k, v) => taxonomy.idsToSubClasses.getOrElse(k, "") -> v
-        })))
+        customers.mapValues(c => Customer(c.id,
+          Spending(c.clSpending.vec.map {
+            case (k, v) => taxonomy.idsToClasses.getOrElse(k, "") -> v
+          }),
+          Spending(c.subClSpending.vec.map {
+            case (k, v) => taxonomy.idsToSubClasses.getOrElse(k, "") -> v
+          }))),
+        fractionalCustomers.mapValues(c => Customer(c.id,
+          Spending(c.clSpending.vec.map {
+            case (k, v) => taxonomy.idsToClasses.getOrElse(k, "") -> v
+          }),
+          Spending(c.subClSpending.vec.map {
+            case (k, v) => taxonomy.idsToSubClasses.getOrElse(k, "") -> v
+          }))),
+        normalizedFractionalCustomers.mapValues(c => Customer(c.id,
+          Spending(c.clSpending.vec.map {
+            case (k, v) => taxonomy.idsToClasses.getOrElse(k, "") -> v
+          }),
+          Spending(c.subClSpending.vec.map {
+            case (k, v) => taxonomy.idsToSubClasses.getOrElse(k, "") -> v
+          })))
       )
     }
 
     val (customers, fractionalCustomers, normalizedFractionalCustomers) =
       computeCustomers
 
-    // Print customers
-    println("\n************ Customers ****************\n")
-    customers.take(5).foreach(println)
-    println("\n******** Fractional Customers *********\n")
-    fractionalCustomers.take(5).foreach(println)
-    println("\n*** Normalized Fractional Customers ***\n")
-    normalizedFractionalCustomers.take(5).foreach(println)
+    // Print customers sample
+    println("\n************ Customers sample ****************\n")
+    customers.take(sampleSize).foreach(println)
+    println("\n******** Fractional Customers sample *********\n")
+    fractionalCustomers.take(sampleSize).foreach(println)
+    println("\n*** Normalized Fractional Customers sample ***\n")
+    normalizedFractionalCustomers.take(sampleSize).foreach(println)
 
     // Display statistics
     //    displayStats(basketsRDD, productsRDD)
--- src/main/scala/project/customer/Customer.scala
+++ src/main/scala/project/customer/Customer.scala
@@ -1,15 +1,17 @@
 package project.customer
 
-case class Customer[A, B](id: A, spending: Spending[B]) {
-  def +(spending: Spending[B]): Customer[A, B] =
-    this.copy(spending = this.spending ++ spending)
+case class Customer[A, B](id: A, clSpending: Spending[B], subClSpending: Spending[B]) {
+  //  def +(spending: Spending[B]): Customer[A, B] =
+  //    this.copy(spending = this.spending ++ spending)
 
   def ++(other: Customer[A, B]): Customer[A, B] =
     if (id == other.id)
-      this.copy(spending = this.spending ++ other.spending)
+      this.copy(clSpending = clSpending ++ other.clSpending,
+        subClSpending = subClSpending ++ other.subClSpending)
     else
       sys.error("Customer.++ with a different ID.")
 
   def fractional: Customer[A, B] =
-    this.copy(spending = this.spending.fractional)
+    this.copy(clSpending = clSpending.fractional,
+      subClSpending = subClSpending.fractional)
 }
